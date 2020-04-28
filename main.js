771e60620b878ef26aaeceac71d264b2cc98a886
Andrierlison <andrierlisonsilva8@gmail.com>
2020-04-28 20:38:38 -0300

    Personalização da interface e adição da área de contato

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-function calcularPorcentagem(){
-    var valor = document.getElementById("valor").value
-    var porcentagem = document.getElementById("porcentagem").value
-    var resultado
-    
-    valor = parseInt(valor)
-    porcentagem = parseInt(porcentagem)
-    
-    resultado = (valor*porcentagem)/100
-    document.getElementById("resultado").innerHTML = resultado
+function calcularPorcentagem() {
+  var valor = document.getElementById("valor").value
+  var porcentagem = document.getElementById("porcentagem").value
+  var resultado
+
+  valor = parseInt(valor)
+  porcentagem = parseInt(porcentagem)
+
+  resultado = (valor * porcentagem) / 100
+  document.getElementById("resultado").innerHTML = porcentagem + '% de ' + valor + ' é ' + resultado
+ 
+  if (isNaN(resultado)) {
+    document.getElementById("resultado").innerHTML = 'O RESULTADO VAI APARECER AQUI'
+  }
 }
